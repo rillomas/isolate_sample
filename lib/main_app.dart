7e8ede4634349d39e653d39fa1a58dd1ff950448
Masato Hori <rillomas@gmail.com>
2016-03-23 11:08:37 +0900

    added code to start isolate

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -2,6 +2,9 @@
 
 library main_app;
 
+import "dart:html";
+import "dart:async";
+import "dart:isolate";
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:logging/logging.dart';
@@ -12,5 +15,22 @@ class MainApp extends PolymerElement {
 
   void ready() {
     Logger.root.info("main-app ready");
+    _startIsolate();
+  }
+
+  _startIsolate() async {
+    var fromIsolate = new ReceivePort();
+    SendPort toIsolate;
+    await Isolate.spawnUri(Uri.parse("heart_beater.dart"), [], fromIsolate.sendPort);
+    fromIsolate.listen((msg) {
+      if (toIsolate == null && msg is SendPort) {
+        toIsolate = msg;
+        toIsolate.send("start");
+        Logger.root.info("started heartbeater");
+      } else {
+        var now = new DateTime.now();
+        Logger.root.info("$msg at $now");
+      }
+    });
   }
 }
